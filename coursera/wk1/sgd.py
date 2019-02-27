ea5f165cd820444c644647aa6c8855b2b246343b
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2019-02-26 21:46:55 -0600

    Added comments and plots

--- coursera/wk1/sgd.py
+++ coursera/wk1/sgd.py
@@ -1,8 +1,13 @@
-import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
-USE_CONCAVE = False
+
+# Used to switch between the two functions to use - True will use a concave/
+# convex function and False will use a sinusoidal. This is used for switching
+# between applying SGD on a function that always converges to the same global
+# minima and one that will converge based on inital conditions to a local
+# minima.
+USE_CONCAVE = True
 
 # Simple parabola
 def func_y_concave(x):
@@ -29,16 +34,16 @@ def cost_function_sine(x):
 # Performs stochastic gradient descent using the above defined cost function
 # Liberally stolen from:
 # https://medium.com/@rrfd/what-is-a-cost-function-gradient-descent-examples-with-python-16273460d634
-def gradient_descent(previous_x, learning_rate, epoch):
+def gradient_descent(initial_guess, learning_rate, epoch):
     x_gd = []
     y_gd = []
     
-    x_gd.append(previous_x)
+    x_gd.append(initial_guess)
     if USE_CONCAVE:
-        y_gd.append(func_y_concave(previous_x))
+        y_gd.append(func_y_concave(initial_guess))
     else:
-        y_gd.append(func_y_sine(previous_x))
-
+        y_gd.append(func_y_sine(initial_guess))
+    previous_x = initial_guess
     for i in range(epoch):
         current_x = 0
         current_y = 0
@@ -56,21 +61,35 @@ def gradient_descent(previous_x, learning_rate, epoch):
 
     return x_gd, y_gd
 
+# Initial guesses to apply for SGD
+initial_guesses = [1.2, 2.3]
 
-initial_guesses = [-1, 6]
+# Learning rates to apply for SGD
 learning_rates = [0.1, 0.2, 0.4]
-num_epochs = 20
+
+# Number of epochs to apply for SGD
+num_epochs = 50
+
+# Configure the plot figure. Note the subplot for each initial guess and
+# learning rate
 gs = gridspec.GridSpec(len(initial_guesses), len(learning_rates))
 fig = plt.figure(tight_layout=True)
 
+# Loop over each initial guess and learning rate and run SGD and plot it
 for i in range(len(initial_guesses)):
     for j in range(len(learning_rates)):
+        # Add this subpot
         ax = fig.add_subplot(gs[i, j])
+
+        # Grab the initial guess and learning rate
         learning_rate = learning_rates[j]
         initial_guess = initial_guesses[i]
+
+        # Run SGD and grab the x, y array of length num_epochs
         x_gd, y_gd = gradient_descent(initial_guess, learning_rate, num_epochs)
-            
-        x = np.linspace(-10, 8, 50)
+        
+        # Grab the x,y values for the function under consideration
+        x = np.linspace(-10, 8, 500)
         y = 0
         if USE_CONCAVE:
             y = [func_y_concave(val) for val in x]
